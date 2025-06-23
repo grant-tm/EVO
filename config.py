984b3d35b9494ec88089a175aa9cba255c00c6e2
grant-tm <grant.t.morgan@gmail.com>
2025-06-22 19:01:10 -0500

    configure for long training session

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ SIM_SPEED = 0.8 # seconds per minute-bar emission
 DATA_PATH = "preprocessed_training_data.csv"
 MODEL_DIR = "trained_models"
 MODEL_NAME = "ppo_trading_agent.zip"
-TRAINING_STEPS = 250_000
+TRAINING_STEPS = 1_000_000
 SEQ_LEN = 15
 FEATURES = [
     "open", "high", "low", "close", "volume",
@@ -52,5 +52,5 @@ MAX_EPISODE_STEPS = 5000
 # GENETIC SEARCH VARIABLES
 MUTATION_RATE = 0.1
 ELITE_PROPORTION = 0.2
-NUM_BACKTESTS = 200
+NUM_BACKTESTS = 500
 BACKTEST_LENGTH = 360
--- run_genetic_search.py
+++ run_genetic_search.py
@@ -2,8 +2,8 @@ from config import MUTATION_RATE, ELITE_PROPORTION
 from evolution_engine.genetic_search import GeneticSearch
 
 ga = GeneticSearch(
-    population_size = 5,
-    max_generations = 5,
+    population_size = 10,
+    max_generations = 25,
     mutation_rate = MUTATION_RATE,
     elite_fraction = ELITE_PROPORTION
 )
