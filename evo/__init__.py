db3b78d4c46deb01d14ce85b3bfe3f398c80c6d8
grant-tm <grant.t.morgan@gmail.com>
2025-07-19 18:10:29 -0500

    add versioning and author info

---
+++ evo/__init__.py
@@ -0,0 +1,15 @@
+"""
+EVO - Neural Network Powered Trading Agent
+
+An experimental reinforcement learning-based trading system using Proximal 
+Policy Optimization (PPO)with genetic hyperparameter and reward function 
+optimization.
+"""
+
+__version__ = "0.1.0"
+__author__ = "grant.t.morgan@gmail.com"
+
+from .core.config import Config
+from .core.logging import setup_logging
+
+__all__ = ["Config", "setup_logging"] 
