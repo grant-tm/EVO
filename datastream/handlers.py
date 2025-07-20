36924470d294a38dbd22d96b0169262f731b5663
grant-tm <grant.t.morgan@gmail.com>
2025-07-20 15:20:32 -0500

    delete prototype files

--- datastream/handlers.py
+++
@@ -1,4 +0,0 @@
-import logging
-
-async def on_minute_bar(bar):
-    print(f"[{bar.timestamp}] {bar.symbol} OHLCV = {bar.open}, {bar.high}, {bar.low}, {bar.close}, {bar.volume}")
--- evolution_engine/__init__.py
+++
@@ -1,1 +0,0 @@
-from .genetic_search import GeneticSearch
