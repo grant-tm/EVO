23719aabf7c393fe613d49da4baea2536fd2027f
grant-tm <grant.t.morgan@gmail.com>
2025-07-24 15:52:29 -0500

    export GenomeConfig

--- evo/optimization/genetic/__init__.py
+++ evo/optimization/genetic/__init__.py
@@ -2,8 +2,15 @@
 Genetic optimization algorithms for hyperparameter tuning and strategy optimization.
 """
 
-from .genome import Genome
+from .genome import Genome, GenomeConfig
 from .genetic_search import GeneticSearch, GeneticSearchConfig
-from .fitness import FitnessEvaluator
+from .fitness import FitnessEvaluator, BacktestFitnessEvaluator
 
-__all__ = ["Genome", "GeneticSearch", "GeneticSearchConfig", "FitnessEvaluator"] 
+__all__ = [
+    "Genome", 
+    "GenomeConfig", 
+    "GeneticSearch", 
+    "GeneticSearchConfig", 
+    "FitnessEvaluator", 
+    "BacktestFitnessEvaluator"
+] 
